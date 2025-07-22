e7c7718c1d4e149bdabb744f7b3d58d4ee36bafa
Ylan <turin-ylan@outlook.fr>
2025-07-22 08:49:28 +0200

    dev: continue work on Verify password added a new link to search passwords from, and a function to resolve strength of password moved index.php to bin/console

--- src/Command/PasswordGeneratorCommand.php
+++ src/Command/PasswordGeneratorCommand.php
@@ -9,9 +9,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-class PasswordGeneratorCommand extends Command
-
-{
+class PasswordGeneratorCommand extends Command {
 
     public function __construct()
     {
